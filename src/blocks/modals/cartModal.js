0ddb70ecf5d675dfb12f2f1e98f48484c0562e5d
mrbek2999 <71260979+mrbek2999@users.noreply.github.com>
2023-09-17 13:24:56 +0500

    Add files via upload

--- src/blocks/modals/cartModal.js
+++ src/blocks/modals/cartModal.js
@@ -1,71 +1,206 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import {connect} from "react-redux";
+import {deleteCart, editMyCart, getAllCart, messageReset} from "../../store/cart";
+import {PatternFormat} from "react-number-format";
+import {ordersSave} from "../../store/Orders";
+import {toast} from "react-toastify";
 import Dialog from '@mui/material/Dialog';
-import ListItemText from '@mui/material/ListItemText';
-import ListItem from '@mui/material/ListItem';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import CloseIcon from '@mui/icons-material/Close';
 import Slide from '@mui/material/Slide';
 import {useEffect, useState} from "react";
 import CloseButton from "react-bootstrap/CloseButton";
+
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function FullScreenDialog(props) {
+function numberWithSpaces(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+function CartModal(props) {
     const [open, setOpen] = useState(false);
-    const [quantity, setQuantity] = useState(1);
+    const [allSumCart, setAllSumCart] = useState(0)
+    const [cart, setCart] = useState([]);
+    const [hasError, setHasError] = useState(false);
+    const [fullName, setFullName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [comment, setComment] = useState('');
 
 
-    function AddQuantity(){
-        setQuantity(quantity+1)
+
+
+    useEffect(() => {
+        props.getAllCart()
+    }, [])
+
+    useEffect(() => {
+        if(!cart.length){
+            handleClose()
+        }
+        setAllSumCart(props.allSumCart)
+    }, [props])
+
+    useEffect(() => {
+        setAllSumCart(props.allSumCart)
+        if (props.message == 'deleted') {
+            props.getAllCart()
+        }
+        if (!props.cart) {
+            props.setIsOpen(false)
+        }
+        let myModalCart = props.cart
+        setCart(myModalCart)
+        props.setCartApp(myModalCart)
+    }, [props.cart])
+
+
+    function AddQuantity(product_item) {
+        let newNum = product_item['quantity']
+        newNum++
+        props.editMyCart({...product_item, quantity: newNum})
     }
-    function RemoveQuantity(){
-        setQuantity(quantity-1)
+
+    function RemoveQuantity(product_item) {
+        let newNum = product_item['quantity']
+        newNum--
+        props.editMyCart({...product_item, quantity: newNum})
+    }
+
+    function changePhoneNumber(n){
+        const myNewNum = n.replace(/[^0-9\d]/ig, "")
+        setPhoneNumber(myNewNum);
     }
 
     function handleClose() {
-        setOpen(false);
         props.setIsOpen(false);
+        setOpen(false);
+    }
+
+    function deleteProduct(item) {
+        props.deleteCart(item)
     }
 
     useEffect(() => {
+        props.getAllCart()
         setOpen(props.isOpen)
     }, [props.isOpen])
 
+    function OrderProducts(e){
+        e.preventDefault()
+        if(phoneNumber.length < 9 || fullName.length < 3){
+            setHasError(true)
+        }else{
+            setHasError(false)
+            const data = {
+                buyer: '11',
+                fullname: fullName,
+                phone_number: "+998"+phoneNumber,
+                comment: comment,
+                orders: cart
+            }
+            props.ordersSave(data)
+            toast.success('Ваш заказ принял!', {theme: 'colored', draggable: false, autoClose:2000, delay: 10})
+            handleClose()
+        }
+    }
 
     return (
         <div>
-            {props.item ?
+            {props.cart ?
                 <Dialog
                     fullScreen
-                    open={open}
+                    open={props.isOpen ? props.isOpen : false}
                     onClose={handleClose}
                     TransitionComponent={Transition}
-                    className={'product-order'}
+                    className={'cart-modal'}
                 >
-                    <img src={props.item.photo} width={100} height={220} alt={props.item.title} />
-                    <h3>{props.item.title}</h3>
-                    <p>{props.item.description}</p>
-                    <div className={'addToCartBlock'}>
-                        <h4 className={'price-product'}>{quantity*props.item.price} сум</h4>
-                        <div className={'counter'}>
-                            <p>Количество</p>
-                            <button onClick={RemoveQuantity} disabled={quantity===1 ? true : false} className={`quantityBtn`}>-</button>
-                            <span className={'quantity'}>{quantity}</span>
-                            <button onClick={AddQuantity} className={'quantityBtn'} >+</button>
-                        </div>
-                        <button className={'addToCartBtn'}>Добавить в корзину</button>
+                    <div className={'cart-header'}>
+                        <h2 className="h2">В корзине {cart ? cart.length : 0} товара</h2>
                     </div>
-                    <CloseButton className={'close-btn'} onClick={handleClose}/>
+                    {cart.map((item, index) =>
+                        <div className={'cart-product'} key={index}>
+                            <div className={'cart-product-photo'}>
+                                <img src={item.product.photo} alt={item.product.title}/>
+                            </div>
+                            <div className={'cart-product-body'}>
+                                <h3>{item.product.title}<DeleteForeverOutlinedIcon onClick={() => deleteProduct(item)}/>
+                                </h3>
+                                <div className={'addToCartBlock quantityBlock'}>
+                                    <h4 className={'price-product'}>{item.product.price * item.quantity} сум</h4>
+                                    <div className={'counter'}>
+                                        <button onClick={() => RemoveQuantity(item)}
+                                                disabled={item.quantity === 1 ? true : false}
+                                                className={`quantityBtn`}>-
+                                        </button>
+                                        <span className={'quantity'}>{item.quantity}</span>
+                                        <button onClick={() => AddQuantity(item)} className={'quantityBtn'}>+</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                    <div className="checkout">
+                        <div className="cart-form__cart-section cart-section">
+                            <div className="cart-section__header">
+                                <h2 className="h2">Ваши данные</h2></div>
+                            <div className="cart-section__body">
+                                <form className="similar-auth" onSubmit={OrderProducts}>
+                                    <div>
+                                        <label className="similar-auth__label">
+                                            Имя и фамилия</label>
+                                        <input type="text" className="input" required min={3}
+                                               onChange={(e)=>setFullName(e.target.value)}/>
+                                        {fullName.length < 3 && fullName
+                                            ? <span role={'alert'} className={'d-block validation-error'}>Длина поля "Имя и фамилия" должна быть не менее 3</span>
+                                            : !fullName && hasError ? <span role={'alert'} className={'d-block validation-error'}>Обязательное поле</span> : ''}
+                                    </div>
+                                    <div>
+                                        <label className="similar-auth__label">
+                                            <span className="">Телефон</span>
+                                        </label>
+                                        <div className="phone-input">
+                                            <div className="phone-input__prefix">
+                                                <div className="phone-input__prefix-value">+998</div>
+                                            </div>
+                                            <PatternFormat
+                                                type="tel"
+                                                className="input _phone"
+                                                format="## ### ## ##"
+                                                mask=" "
+                                                onValueChange={value => changePhoneNumber(value.formattedValue)}
+                                                required
+                                            />
+                                        </div>
+                                        {phoneNumber.length !== 9 && phoneNumber
+                                            ? <span role={'alert'} className={'d-block validation-error'}>Длина поля номера телефона должна быть 9</span>
+                                            : !phoneNumber && hasError ? <span role={'alert'} className={'d-block validation-error'}>Обязательное поле</span> : ''}
+                                    </div>
+                                    <div>
+                                        <label className="similar-auth__label">
+                                            Комментарий</label>
+                                        <textarea className="input"
+                                               onChange={(e)=>setComment(e.target.value)}/>
+                                    </div>
+                                    <div className="checkout-price">
+                                        <h4 className="cart-form__sidebar-price-label">Итого:</h4>
+                                        <div className="price">
+                                            <h4 className="price__main">{numberWithSpaces(allSumCart)} сум</h4>
+                                        </div>
+                                    </div>
+                                    <button type="submit" className="btn btn__red btn__big _fw btn-order">Оформить заказ</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <CloseButton onClick={handleClose} className={'close-btn'}/>
                 </Dialog>
-                : <div>Loading...</div>}
+                : ''}
         </div>
     );
 }
+export default connect(props => props.cart, {
+    getAllCart, deleteCart,
+        editMyCart, ordersSave, messageReset
+})(CartModal)
--- src/blocks/modals/productModal.js
+++ src/blocks/modals/productModal.js
@@ -2,18 +2,50 @@ import {useEffect, useState} from 'react';
 import Button from '@mui/material/Button';
 import Modal from 'react-bootstrap/Modal';
 import CloseIcon from '@mui/icons-material/Close';
+import {connect} from "react-redux";
+import {cartSave, getAllCart, messageReset} from "../../store/cart";
+import {toast, ToastContainer} from "react-toastify";
 
 
 function ProductModal(props) {
     const [show, setShow] = useState(false);
     const [quantity, setQuantity] = useState(1);
 
+    useEffect(() => {
+        if (props.message == 'deleted') {
+            toast.success('Товар удален из корзины!', {theme: 'colored', draggable: false, autoClose: 1600, delay: 10})
+            props.messageReset()
+        } else if (props.message === 'error') {
+            toast.error('Ошибка! Пожалуйста, обновите страницу!', {theme: 'colored', draggable: false, autoClose: 1600})
+            props.messageReset()
+        } else if (props.message === true) {
+            toast.success('Товары добавлены в корзину', {theme: 'colored', draggable: false, autoClose: 1600, delay: 10})
+            props.messageReset()
+            props.getAllCart()
+            props.setIsOpen(false)
+        } else if (props.message === 'edited') {
+            props.messageReset()
+        }
+    }, [props.message])
 
-    function AddQuantity(){
-        setQuantity(quantity+1)
+    useEffect(() => {
+        setQuantity(1)
+    }, [props])
+
+    function addToCart(item) {
+        let data = {};
+        data.buyer = 11
+        data.quantity = quantity
+        data.product = item.id
+        props.cartSave(data)
     }
-    function RemoveQuantity(){
-        setQuantity(quantity-1)
+
+    function AddQuantity() {
+        setQuantity(quantity + 1)
+    }
+
+    function RemoveQuantity() {
+        setQuantity(quantity - 1)
     }
 
     function handleClose() {
@@ -27,27 +59,33 @@ function ProductModal(props) {
 
     return (
         <div>
-            {props.item ?
-                <Modal show={show} onHide={handleClose} className={'product-order mt-3'} aria-labelledby="contained-modal-title-vcenter">
-                    <img src={props.item.photo} width={100} height={220} alt={props.item.title} />
-                    <h3>{props.item.title}</h3>
-                    <p>{props.item.description}</p>
+            <ToastContainer/>
+            {props.orderItem ?
+                <Modal show={show} onHide={handleClose} className={'product-order mt-3'}
+                       aria-labelledby="contained-modal-title-vcenter">
+                    <img src={props.orderItem.photo} width={100} height={220} alt={props.orderItem.title}/>
+                    <h3>{props.orderItem.title}</h3>
+                    <p>{props.orderItem.description}</p>
                     <div className={'addToCartBlock'}>
-                        <h4 className={'price-product'}>{quantity*props.item.price} сум</h4>
+                        <h4 className={'price-product'}>{quantity * props.orderItem.price} сум</h4>
                         <div className={'counter'}>
                             <p>Количество</p>
-                            <button onClick={RemoveQuantity} disabled={quantity===1 ? true : false} className={`quantityBtn`}>-</button>
+                            <button onClick={RemoveQuantity} disabled={quantity === 1 ? true : false}
+                                    className={`quantityBtn`}>-
+                            </button>
                             <span className={'quantity'}>{quantity}</span>
-                            <button onClick={AddQuantity} className={'quantityBtn'} >+</button>
+                            <button onClick={AddQuantity} className={'quantityBtn'}>+</button>
                         </div>
-                        <button className={'addToCartBtn'}>Добавить в корзину</button>
+                        <button className={'addToCartBtn'}
+                                onClick={() => addToCart(props.orderItem)}>Добавить в корзину
+                        </button>
                     </div>
-                    <Button variant="contained"  className={'close-btn'} onClick={handleClose}><CloseIcon/></Button>
-                </Modal>
-                :
-                <div>Loading...</div>}
+                    <Button variant="contained" className={'close-btn'}
+                            onClick={handleClose}><CloseIcon/></Button>
+                </Modal> : ''}
+
         </div>
-    );
+    )
 }
 
-export default ProductModal;
+export default connect(props => props.cart, {cartSave, messageReset, getAllCart})(ProductModal)
